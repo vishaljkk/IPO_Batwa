47f007880a10fffd680545a196500088768ca9df
vishaljkk <vishal.kk@somaiya.edu>
2021-07-26 10:03:19 +0530

    break the application into specific components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { BalanceInvested } from './components/BalanceInvested';
+import { CurrentBid } from './components/CurrentBid';
+import { AddBid } from './components/AddBid';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Balance />
+      <BalanceInvested/>
+      <CurrentBid/>
+      <AddBid/>
     </div>
   );
 }
